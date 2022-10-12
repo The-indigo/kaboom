27d174700f634e1a4bf990107fb91c1dd39a70e6
The-indigo <adepojuadeyemi11@gmail.com>
2022-10-12 10:16:01 -0400

    collections page

--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -16,16 +16,15 @@ const Collections = () => {
 
             <section className="collections-image-section">
                 <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"} />
-                <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"} />
-                <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"} />
-                    <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"}/>
+                <CollectionsItem imageSource={"images/lead.png"} name="Limits" artist={"John Watts"} />
+                <CollectionsItem imageSource={"images/blind.png"} name="Limits" artist={"John Watts"} />
+                    <CollectionsItem imageSource={"images/black.png"} name="Limits" artist={"John Watts"}/>
 
-    <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"}/>
+    <CollectionsItem imageSource={"images/sun.png"} name="Limits" artist={"John Watts"}/>
                     <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"}/>
-    <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"}/>
-    <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"}/>
-    <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"}/>
-    <CollectionsItem imageSource={"images/fly.png"} name="Limits" artist={"John Watts"}/>
+                <CollectionsItem imageSource={"images/alpha.png"} name="Limits" artist={"John Watts"} />
+                
+ 
 
 
 
